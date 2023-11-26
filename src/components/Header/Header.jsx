7c4987f58dbf7ab5e640d156325401569ff82725
razikhaNawaz <razikhafaheem02@gmail.com>
2023-11-26 17:34:41 +0530

    vv

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,33 +1,30 @@
-import React, { useState } from 'react'
-import './Header.css'
-import { BiMenuAltRight } from "react-icons/bi";
-import { getMenuStyles } from "../../utils/common";
-// import useHeaderColor from "../../hooks/useHeaderColor";
-import OutsideClickHandler from "react-outside-click-handler";
+import React, { useState } from 'react';
+import './Header.css';
+import { BiMenuAltRight } from 'react-icons/bi';
+import { getMenuStyles } from '../../utils/common';
+import OutsideClickHandler from 'react-outside-click-handler';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
-  // const headerColor = useHeaderColor();
+
   return (
     <section className="h-wrapper">
-      <div className='flexCenter paddings innerWidth h-container'>
+      <div className="flexCenter paddings innerWidth h-container">
         <img src="./logo.png" alt="logo" width={100} />
         <OutsideClickHandler
           onOutsideClick={() => {
             setMenuOpened(false);
           }}
-          >
-        <div className='flexCenter h-menu' style={getMenuStyles(menuOpened)}>
-          <a href="about">About Us</a>
-          <a href="home">Home</a>
-          <a href="clients">Our Clients</a>
-          <a href="previous">Previous projects</a>
-          <a href="services">Our Services</a>
-          
-            <a href="contact">Contact us</a>
-          
-          
-        </div>
+        >
+          <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
+            <Link to="/about">About Us</Link>
+            <Link to="/">Home</Link>
+            <Link to="/clients">Our Clients</Link>
+            <Link to="/previous">Previous projects</Link>
+            <Link to="/services">Our Services</Link>
+            <Link to="/contact">Contact us</Link>
+          </div>
         </OutsideClickHandler>
         {/* for medium and small screens */}
         <div
@@ -37,9 +34,8 @@ const Header = () => {
           <BiMenuAltRight size={30} />
         </div>
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
